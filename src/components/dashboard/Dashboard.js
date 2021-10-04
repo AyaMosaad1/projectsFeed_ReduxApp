13c59c421c7e3f84efcb0b705ca4de08791880a7
Aya Mosaad Mohamed <36517014+AyaMosaad1@users.noreply.github.com>
2021-10-04 06:01:17 +0200

    get data from firebaseStore to component in app

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import ProjectList from '../projects/ProjectList'
 import Notifications from './Notifications'
 import { connect } from 'react-redux'
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
 
 class Dashboard extends Component {
   render() {
-    
+
     // console.log(this.props);
     const { projects } = this.props;
-    
+
     return (
       <div className="dashboard container">
         <div className="row">
@@ -26,8 +28,13 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose (
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection : 'projects'}
+  ])
+)(Dashboard)
